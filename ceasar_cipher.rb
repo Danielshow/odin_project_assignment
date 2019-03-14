2fa2a242335613a118ba2272db42b7a47cbc3499
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-03-14 17:02:54 +0100

    complete cipher

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,10 +1,35 @@
 def build_hash
-  letters = ('a'..'z').to_a 
+  lowerletters = ('a'..'z').to_a
+  upperletters = ('A'..'Z').to_a 
   hash = Hash.new
-  letters.each do |letter|
+  lowerletters.each do |letter|
+    hash[letter] = letter.ord
+  end
+
+  upperletters.each do |letter|
     hash[letter] = letter.ord
   end
   hash
 end
 
-p build_hash
+def ceasar_cipher(sentence, step)
+  hashLetters = build_hash
+  hash_string = ''
+  for i in sentence.split('')
+    if hashLetters.has_key?(i)
+      if hashLetters[i] + step > 122 and hashLetters[i] >= 97
+        numberHash = (hashLetters[i] + step - 123)
+        hash_string += hashLetters.key(numberHash + 97)
+      elsif hashLetters[i] + step > 90 and hashLetters[i] >= 65 and hashLetters[i] <= 90
+        numberHash = (hashLetters[i] + step) - 91
+        hash_string += hashLetters.key(numberHash + 65)
+      else
+        hash_string += hashLetters.key(hashLetters[i]+step)
+      end 
+    else
+      hash_string+=i
+    end
+  end
+  hash_string
+end
+p ceasar_cipher("", 5)
