13eb65492d3d08e5ac3444d4fe7700ac40e3768c
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-03-17 12:29:28 +0100

    completed inject enumerable method

--- enumerable.rb
+++ enumerable.rb
@@ -79,10 +79,35 @@ module Enumerable
     self.my_each{|x| newArray << yield(x)}
     newArray
   end
+
+  def my_inject(arg=nil)
+    return self.to_enum unless block_given?
+    answer = nil
+    if arg != nil 
+      answer = arg
+      self.my_each {|n| answer = yield(answer, n)}
+    else 
+      answer = self.to_a[0]
+      self.my_each_with_index {|n, index| answer = yield(answer, self.to_a[index+1]) if index+1 < self.to_a.length}
+    end
+    answer
+  end 
 end
 
+# a method that use my created inject
+def multiply_els arr
+  arr.my_inject(1) {|product, n| product * n}
+end 
 
+p multiply_els([2,4,5])
+
+p (5..10).my_inject(10) {|sum, n| sum - n }            #=> 45
+p (5..10).my_inject(10) {|product, n| product * n }
 #nums = [3,2,1,8,9,9,5,4]
+longest = %w{ cat sheep bear }.my_inject do |memo,word|
+   memo.length > word.length ? memo : word
+end
+p longest                                       #=> "sheep"
 # letter = ["iam", "goan", "dgdhj", "dyhddn", "dhgdgdh"]
 
 #nums.my_each {|n| puts n}
@@ -92,10 +117,12 @@ end
 # puts letter.my_all? {|num| num.length >= 3}
 # p %w{ajn b ct}.my_any? {|word| word.length >= 3} 
 # p %w{ant bear catjj}.my_none? {|word| word.length == 5}
- ary = [1, 2, 4, 2]
-# p ary.my_count { |x| x%2 == 0 }
+ # ary = [1, 2, 4, 2]
+# p arymy_count { |x| x%2 == 0 }
 
 # p ary.my_count(4)
 #
 #
-p ary.map
+# p ary.map
+
+
